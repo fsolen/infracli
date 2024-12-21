a4c8c12bc1b8c0a23d58d45f05f3f4bf4493eb97
Fatih Solen <50904951+fsolen@users.noreply.github.com>
2024-12-21 16:01:38 +0300

    Update cloudstack_manager.py

--- managers/cloudstack_manager.py
+++ managers/cloudstack_manager.py
@@ -1,12 +1,14 @@
 import os
 import yaml
 from cs import CloudStack
+from .phpipam_manager import PhpIpamManager
 
 class CloudStackManager:
     def __init__(self, config_path):
         self.config_path = config_path
         self.clusters = self.load_clusters()
         self.vm_count = {}  # Dictionary to keep track of VM counts for each cluster
+        self.phpipam_manager = PhpIpamManager(config_path)
 
     def load_clusters(self):
         clusters = {}
@@ -19,12 +21,21 @@ class CloudStackManager:
                     self.vm_count[cluster_name] = 0  # Initialize VM count for the cluster
         return clusters
 
+    def allocate_ip(self, vm_profile):
+        vlan_name = vm_profile.get('vlan')
+        if vlan_name:
+            ip_address = self.phpipam_manager.get_next_available_ip(vlan_name)
+            return ip_address
+        else:
+            raise ValueError("VLAN not specified in vm_profile")
+
     def create_vm(self, cluster_name, profile):
         cluster = self.clusters.get(cluster_name)
         if cluster:
             self.vm_count[cluster_name] += 1  # Increment VM count for the cluster
             vm_index = self.vm_count[cluster_name]
             vm_name = profile['hostname_pattern'].format(index=vm_index)
+            ip_address = self.allocate_ip(profile)
             vm_config = {
                 'serviceofferingid': profile['service_offering'],
                 'templateid': profile['template_name'],
@@ -35,7 +46,7 @@ class CloudStackManager:
                 'cpu': profile['cpu'],
                 'memory': profile['memory'],
                 'disklist': [{'name': disk['name'], 'size': disk['size_gb']} for disk in profile['disks']],
-                'ipaddress': profile['ip_settings']['ip_address'],
+                'ipaddress': ip_address,
                 'netmask': profile['ip_settings']['subnet_mask'],
                 'gateway': profile['ip_settings']['default_gateway'],
                 'dns1': profile['ip_settings']['dns_servers'][0],
