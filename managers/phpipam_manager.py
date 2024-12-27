55300e1c6ce3c9e02b42194edaea50e61993b06b
Fatih Solen <50904951+fsolen@users.noreply.github.com>
2024-12-28 01:54:03 +0300

    Update phpipam_manager.py

--- managers/phpipam_manager.py
+++ managers/phpipam_manager.py
@@ -1,12 +1,11 @@
 import requests
 import yaml
-from .site_config import SiteConfig
 from .vault_manager import VaultManager
 
 class PhpIpamManager:
-    def __init__(self, site_name, config_path):
-        self.site_config = SiteConfig(config_path).get_site_config(site_name)
-        self.vault_manager = VaultManager(site_name, config_path)
+    def __init__(self, site_config):
+        self.site_config = site_config
+        self.vault_manager = VaultManager(site_config)
         self.credentials = self.vault_manager.read_secret(self.site_config['vault_path'])
         self.base_url = self.site_config['phpipam']['base_url']
         self.app_id = self.credentials['app_id']
